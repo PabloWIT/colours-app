b44c13993b129d860cc4c483d681830e272c8771
pablogodiaz <pablogodiaz+1@gmail.com>
2022-02-10 16:42:04 +0000

    Added coloursEndingWithLetter testing in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -50,4 +50,5 @@ fun main(args: Array<String>){
     //    Colour: Silver
     colours.getAll().forEach { println("Colour: $it") }
     //TODO for the three additional functions you wrote in Colours.kt, print the returned value here.
+    println("Colours ending with specific letter ${colours.coloursEndingWithLetter('e')} [ e was chosen ]")
 }
